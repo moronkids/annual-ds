9b681827ffbe32d04214fd88f91c24df0167c0ad
Sulistyo <sulistyoardani@gmail.com>
2021-12-13 14:48:45 +0700

    fix: section container

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f837019b52aeed141077"; }
+/******/ 		__webpack_require__.h = function() { return "29169c74beec8bf6a7b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
