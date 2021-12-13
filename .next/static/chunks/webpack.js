390326b5c4f172e1412586d3006c0cb89f0ffc9a
Sulistyo <sulistyoardani@gmail.com>
2021-12-13 12:22:48 +0700

    fix: lost coed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "78b4047e9a370290c893"; }
+/******/ 		__webpack_require__.h = function() { return "27ab8c71fe41874b3276"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
