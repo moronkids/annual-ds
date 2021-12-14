d0dbab4f159ec4b7b4f0c7c5ced8e2a8faf6e1cd
Sulistyo <sulistyoardani@gmail.com>
2021-12-14 13:52:56 +0700

    fix: slider x2 fund

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "42aef7737a3235794aaa"; }
+/******/ 		__webpack_require__.h = function() { return "4ab3c1dcc6bd02d3a0d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
